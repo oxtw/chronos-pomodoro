16caac426554f1f976bdcf6b5c94cd01ff1bb9dd
miguel <weigertmiguel@hotmail.com>
2025-07-24 12:09:14 -0300

    chore: criando componente React jsx tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <h1>Hello, World!</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias
+      tempore saepe illo distinctio dolores soluta iusto, minima amet ad
+      temporibus ullam, fugit, aliquam doloribus maxime suscipit ipsam
+      voluptatibus rerum nihil!
+    </p>
   </StrictMode>,
-)
+);
