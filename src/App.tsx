ae4b32b6ea41297c3d01ea4df47835e13282739c
miguel <weigertmiguel@hotmail.com>
2025-07-31 13:26:58 -0300

    refactor: movendo Container para componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export default function App() {
   console.log('App component rendered');
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      
-      <div className='container'>
-        <div className='content'>
-          <section>MENU</section>
-        </div>
-      </div>
+      <Container>
+       <Heading>LOGO</Heading>
+      </Container>
 
-      <div className='container'>
-        <div className='content'>
-          <section>FORM</section>
-        </div>
-      </div>
+      <Container>
+       <Heading>MENU</Heading>
+      </Container>
 
-      <div className='container'>
-        <div className='content'>
-          <section>FOOTER</section>
-        </div>
-      </div>
     </>
   );
 }
