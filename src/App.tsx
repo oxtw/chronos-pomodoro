a7b8bd7c304a1f4ae8c9dbd8580d4a26c7f74dca
miguel <weigertmiguel@hotmail.com>
2025-08-06 16:00:40 -0300

    feat: criando CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -15,6 +16,10 @@ export default function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
