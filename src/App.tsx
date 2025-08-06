2d05672dba6050456faaf7ee8bb76a03f269a7c7
miguel <weigertmiguel@hotmail.com>
2025-08-06 15:18:33 -0300

    feat: criando menu com links e ícones

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -14,7 +14,7 @@ export default function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
