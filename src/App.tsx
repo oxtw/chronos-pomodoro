446fb76707a41b209cf7ba0021879b3bbd4ecec4
miguel <weigertmiguel@hotmail.com>
2025-08-06 14:46:34 -0300

    feat: criando logo com ícone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import './styles/theme.css';
-import './styles/global.css';
-
 import { Heading } from './components/Heading';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export default function App() {
   console.log('App component rendered');
   return (
     <>
       <Container>
-       <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
-       <Heading>MENU</Heading>
+        <Heading>MENU</Heading>
       </Container>
-
     </>
   );
 }
