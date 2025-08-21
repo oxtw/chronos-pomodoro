19f8a4c32978549a5979d9e07e85c08dd551b4db
miguel <weigertmiguel@hotmail.com>
2025-08-21 14:15:55 -0300

    feat: criando footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { CountDown } from './components/CountDown';
 import { Cycles } from './components/Cycles';
 import { DefaultInput } from './components/DefaultInput';
 import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -42,14 +43,17 @@ export default function App() {
           </div>
 
           <div className='formRow'>
-           <Cycles />
+            <Cycles />
           </div>
 
           <div className='formRow'>
-            <DefaultButton color='green' icon={<PlayCircleIcon/>}/>
-            <DefaultButton color='red' icon={<StopCircleIcon/>}/>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
