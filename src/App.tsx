9d4f82dc3d65ca9d780afb5a870fbc6784c9adab
miguel <weigertmiguel@hotmail.com>
2025-08-26 14:54:28 -0300

    feat: criando componente GenericHtml

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Home from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export default function App() {
-  return <Home />;
+  return <Home/>;
 }
