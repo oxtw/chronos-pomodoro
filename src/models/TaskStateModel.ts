e95ee7123c19651f5916734fbc8720854bed656f
miguel <weigertmiguel@hotmail.com>
2025-08-27 12:03:09 -0300

    refactor: separando componentes com contexto

--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -1,14 +1,16 @@
 import type { TaskModel } from "./TaskModel";
 
+//Estado -> Componente -> Filhos
+
 export type TaskStateModel = {
-  tasks: TaskModel[];
-  secondsRemaining: number;
-  formattedSecondsRemaining: string;
-  activeTask: TaskModel | null;
-  currentCycle: number; // 1 a 8
+  tasks: TaskModel[]; //Historico, MainForm
+  secondsRemaining: number; // Home, Countdown, Historico, MainForm, Button
+  formattedSecondsRemaining: string;// Titulo, Countdown
+  activeTask: TaskModel | null;//Countdown, Historico, MainForm, Button
+  currentCycle: number; //Home
   config: {
-    workTime: number;
-    shortBreakTime: number;
-    longBreakTime: number;
+    workTime: number;// MainForm
+    shortBreakTime: number;// MainForm
+    longBreakTime: number;// MainForm
   };
 }
