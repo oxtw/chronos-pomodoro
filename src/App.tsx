01ad0631ece0189bfc55ec1ccd064af4f7dcafa5
miguel <weigertmiguel@hotmail.com>
2025-08-28 15:52:21 -0300

    refactor: Context API com useContext e useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import type { TaskStateModel } from './models/TaskStateModel';
-import { useState } from 'react';
+import { TaskContextProvider } from './contexts/TaskContext';
 import Home from './pages/Home';
 
 import './styles/global.css';
 import './styles/theme.css';
-import { TaskContext } from './contexts/TaskContext';
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 export default function App() {
-  const [state, setState] = useState(initialState);
 
   return (
-    <TaskContext.Provider value={{outraCoisa: 321}}>
+    <TaskContextProvider>
       <Home />
-    </TaskContext.Provider>
+    </TaskContextProvider>
   );
 }
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,13 +1,9 @@
 import styles from './styles.module.css';
-import { useContext } from 'react';
-import { TaskContext } from '../../contexts/TaskContext/index';
+import { useTaskContext } from '../../contexts/TaskContext/index';
 
 export function CountDown() {
-  const taskContext = useContext(TaskContext);
-  console.log(taskContext);
+  const { state } = useTaskContext();
   {
-    return (
-      <div className={styles.container}>00:00</div>
-    );
+    return <div className={styles.container}>{state.formattedSecondsRemaining}</div>;
   }
 }
