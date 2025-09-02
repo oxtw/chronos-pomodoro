0e18293d6d8f83a619fd9d816f15ac3977ff05ac
miguel <weigertmiguel@hotmail.com>
2025-09-02 14:01:48 -0300

    feat: input controlado com value e onChange

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,22 +2,29 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
+
 
 export function MainForm() {
 
+  const [taskName, setTaskName] = useState('');
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>){
     event.preventDefault();
-    console.log('nova tarefa');
+    console.log('nova tarefa', taskName);
   }
 
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
+      <h1>{taskName}</h1>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
           type='text'
           placeholder='Digite Algo...'
+          value={taskName}
+          onChange={ e => setTaskName(e.target.value)}
         />
       </div>
 
