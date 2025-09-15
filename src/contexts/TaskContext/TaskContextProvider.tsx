aac86dc5e07cf2bf063b000e1c253eb81b400667
miguel <weigertmiguel@hotmail.com>
2025-09-15 16:18:45 -0300

    feat: atualizando contador a cada segundo

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -3,6 +3,7 @@ import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
+import { TaskActionTypes } from './TaskActions';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -18,8 +19,15 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     console.log(countDownSeconds);
 
     if (countDownSeconds <= 0) {
-      console.log('Worker COMPLETED');
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
       worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
     }
   });
 
