90d42ad657062a1c096960a296087a518ada9b09
miguel <weigertmiguel@hotmail.com>
2025-09-18 16:22:13 -0300

    feat: criando RouterLink

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,15 @@
-import { Link } from 'react-router';
 import styles from './styles.module.css';
+import { RouterLink } from '../RouterLink';
 
 export function Footer() {
-  return <footer className={styles.footer}>
-    <Link to="/about-pomodoro/">Entenda como funciona a técnica pomodoro</Link>
-    <Link to="/">Chornos Pomodoro &copy; {new Date().getFullYear()} - Feito com ❤️ </Link>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <RouterLink href='/about-pomodoro/'>
+        Entenda como funciona a técnica pomodoro
+      </RouterLink>
+      <RouterLink href='/'>
+        Chornos Pomodoro &copy; {new Date().getFullYear()} - Feito com ❤️{' '}
+      </RouterLink>
+    </footer>
+  );
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,9 +6,9 @@ import {
   SunIcon,
 } from 'lucide-react';
 import { useState, useEffect } from 'react';
+import { RouterLink } from '../RouterLink';
 
 import styles from './styles.module.css';
-import { Link } from 'react-router';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -42,31 +42,34 @@ export function Menu() {
 
   return (
     <nav className={styles.menu}>
-      <Link
+      <RouterLink
         className={styles.menuLink}
-        to='/'
+        href='/'
         aria-label='Ir para a Home'
         title='Ir para a Home'
       >
         <HouseIcon />
-      </Link>
-      <a
+      </RouterLink>
+
+      <RouterLink
         className={styles.menuLink}
-        href='#'
+        href='/history/'
         aria-label='Ver Histórico'
         title='Ver Histórico'
       >
         <HistoryIcon />
-      </a>
-      <a
+      </RouterLink>
+
+      <RouterLink
         className={styles.menuLink}
-        href='#'
+        href='/settings/'
         aria-label='Configurações'
         title='Configurações'
       >
         <SettingsIcon />
-      </a>
-      <a
+      </RouterLink>
+      
+      <RouterLink
         className={styles.menuLink}
         href='#'
         aria-label='Mudar Tema'
@@ -74,7 +77,7 @@ export function Menu() {
         onClick={handleThemeChange}
       >
         {nextThemeIcon[theme]}
-      </a>
+      </RouterLink>
     </nav>
   );
 }
